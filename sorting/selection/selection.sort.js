f780474bbdb77bba461ba63fff380e39990ae9ee
maktarsis <tarsis.maksym@gmail.com>
2019-09-11 15:59:38 +0300

    add selection sort

---
+++ sorting/selection/selection.sort.js
@@ -0,0 +1,18 @@
+export const sortBySelection = array => {
+    const n = array.length;
+
+    for (let i = 0; i < n; i += 1) {
+        let minId = i;
+        for (let j = i + 1; j < n; j += 1) {
+            if (array[j] < array[minId]) {
+                minId = j;
+            }
+        }
+
+        const temp = array[i];
+        array[i] = array[minId];
+        array[minId] = temp;
+    }
+
+    return array;
+};
