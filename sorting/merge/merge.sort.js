f0faa4b15816cee3ef964472dc3eaf78f25df9ca
maktarsis <tarsis.maksym@gmail.com>
2019-09-12 13:13:30 +0300

    add merge sorting

---
+++ sorting/merge/merge.sort.js
@@ -0,0 +1,33 @@
+export const sortByMerge = array => {
+    if (array.length === 1) return array;
+
+    const divideId = Math.floor(array.length / 2);
+    const left = array.slice(0, divideId);
+    const right = array.slice(divideId);
+
+    return merge(
+        sortByMerge(left),
+        sortByMerge(right)
+    );
+};
+
+function merge(left, right) {
+    const sortedArray = [];
+    let leftId = 0;
+    let rightId = 0;
+
+    while(leftId < left.length && rightId < right.length) {
+        if (left[leftId] < right[rightId]) {
+            sortedArray.push(left[leftId]);
+            leftId += 1;
+        } else {
+            sortedArray.push(right[rightId]);
+            rightId += 1;
+        }
+    }
+
+    return sortedArray.concat(
+        left.slice(leftId),
+        right.slice(rightId)
+    )
+}
---
+++ sorting/merge/merge.test.js
@@ -0,0 +1,24 @@
+import { sortByMerge } from './merge.sort';
+
+describe('[SORT] merge', () => {
+    test('should sort array ', () => {
+        expect(sortByMerge([
+            1,3,5,4,2
+        ])).toEqual([
+            1,2,3,4,5
+        ]);
+    });
+
+    test('should return already sorted array without changes', () => {
+        const array = [1,2,3,4,5];
+        expect(sortByMerge(array)).toEqual(array);
+    });
+
+    test('should return sorted array when array is sorted in reverse order', () => {
+        expect(sortByMerge([
+            5,4,3,2,1
+        ])).toEqual([
+            1,2,3,4,5
+        ]);
+    });
+});
