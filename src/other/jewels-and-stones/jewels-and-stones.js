0f3a8b6b89974cbb7ce12876c426f4bdfd2c5a99
maktarsis <tarsis.maksym@gmail.com>
2019-09-13 17:25:40 +0300

    add new algorithm solution

---
+++ src/other/jewels-and-stones/jewels-and-stones.js
@@ -0,0 +1,19 @@
+/**
+ * Leetcode #771
+ * https://leetcode.com/problems/jewels-and-stones/
+ */
+/**
+ * @param {string} stoneTypes
+ * @param {string} stonesInStock
+ * @return {number}
+ */
+export const numJewelsInStones = (stoneTypes, stonesInStock) => {
+    const stoneTypesMap = stoneTypes.split('').reduce((types, type) => {
+        types[type] = true;
+        return types;
+    }, {});
+
+    return stonesInStock
+        .split('')
+        .reduce((numJewels, stoneInStock) => stoneTypesMap[stoneInStock] ? numJewels + 1 : numJewels, 0)
+};
---
+++ src/other/jewels-and-stones/jewels-and-stones.test.js
@@ -0,0 +1,27 @@
+import {numJewelsInStones} from "./jewels-and-stones";
+
+describe('[OTHER] jewels and stones', () => {
+    test('should return base leetcode result', () => {
+        expect(numJewelsInStones('aA', 'aAAbbbb')).toBe(3)
+    });
+
+    test('should return case sensitive result', () => {
+        expect(numJewelsInStones('z', 'ZZ')).toBe(0)
+    });
+
+    test('should return basic leetcode result', () => {
+        expect(numJewelsInStones('aA', 'aAAbbbb')).toBe(3)
+    });
+
+    test('should return 0 when no such types', () => {
+        expect(numJewelsInStones('aAbB', 'cCdD')).toBe(0)
+    });
+
+    test('should return one type when there is only one appropriate equivalent', () => {
+        expect(numJewelsInStones('aAbB', 'acCdD')).toBe(1)
+    });
+
+    test('should return appropriate amount of stones if there is equivalent stone', () => {
+        expect(numJewelsInStones('aAbB', 'aAaa')).toBe(4);
+    });
+});
