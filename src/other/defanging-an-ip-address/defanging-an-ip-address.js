7b4c4988f0bf5e8083040f9f476c1f8270c77ef9
maktarsis <tarsis.maksym@gmail.com>
2019-09-16 15:57:41 +0300

    added new algorithm to solve defanging-an-ip-address

---
+++ src/other/defanging-an-ip-address/defanging-an-ip-address.js
@@ -0,0 +1,21 @@
+/**
+ * Leetcode #1108
+ * https://leetcode.com/problems/defanging-an-ip-address/
+ */
+/**
+ * @param {string} address
+ * @return {string}
+ */
+export const defangIPAddress = address => {
+    let resultString = '';
+
+    for (let i = 0; i < address.length; i++) {
+        if (address[i] !== '.') {
+            resultString += address[i];
+        } else {
+            resultString += '[.]';
+        }
+    }
+
+    return resultString;
+};
