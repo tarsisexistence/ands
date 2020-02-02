bf4066b96088879c99b3d18f3f8805d49e932d73
maktarsis <tarsis.maksym@gmail.com>
2020-02-02 23:26:09 +0200

    add binary search solution

---
+++ src/leetcode/search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
@@ -0,0 +1,38 @@
+/**
+ * Leetcode #700
+ * Difficulty: Easy
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+const searchBST = (root, val) => {
+  if (root !== null) {
+    if (root.val === val) {
+      return root;
+    }
+
+    const leftSubtree = searchBST(root.left, val);
+
+    if (leftSubtree) {
+      return leftSubtree;
+    }
+
+    const rightSubtree = searchBST(root.right, val);
+
+    if (rightSubtree) {
+      return rightSubtree;
+    }
+  }
+
+  return null;
+};
