d906495dc8cd22ce7e71bc17e8ec5d0d80065684
maktarsis <tarsis.maksym@gmail.com>
2020-03-07 18:29:27 +0200

    refactor bubble sort

--- src/sorting/bubble/bubble.sort.js
+++ src/sorting/bubble/bubble.sort.js
@@ -3,32 +3,32 @@
  * Complexity: O(N^2)
  */
 export const sortByBubble = input => {
-    const array = [...input];
-    let { length: lastStable } = array;
-    let isSwapped;
+  const array = [...input];
+  let lastStable = array.length;
+  let hasSwapped = false;
 
-    while (lastStable !== 0) {
-        isSwapped = false;
+  while (lastStable > 0) {
+    hasSwapped = false;
 
-        for (let i = 0; i < lastStable; i += 1) {
-            if (array[i] > array[i + 1]) {
-                isSwapped = true;
-                const temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-            }
-        }
+    for (let i = 0; i < lastStable; i += 1) {
+      if (array[i] > array[i + 1]) {
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
 
-        if (isSwapped === false) {
-            return array;
-        }
-
-        lastStable -= 1;
+        hasSwapped = true;
+      }
     }
 
-    return array;
-};
+    if (!hasSwapped) {
+      return array;
+    }
 
+    lastStable -= 1;
+  }
+
+  return array;
+};
 
 /**
  * Inverted version of
@@ -36,26 +36,26 @@ export const sortByBubble = input => {
  * Complexity: O(N^2)
  */
 export const sortByInvertedBubble = input => {
-    const array = [...input];
-    let hasSwapped = false;
+  const array = [...input];
+  let hasSwapped = false;
 
-    for (let i = 0; i < array.length; i += 1) {
-        hasSwapped = false;
+  for (let i = 0; i < array.length; i += 1) {
+    hasSwapped = false;
 
-        for (let j = array.length - 1; j > i; j -= 1) {
-            if (array[j] < array[j - 1]) {
-                hasSwapped = true;
-                const temp = array[j];
-                array[j] = array[j - 1];
-                array[j - 1] = temp;
-            }
-        }
+    for (let j = array.length - 1; j > i; j -= 1) {
+      if (array[j] < array[j - 1]) {
+        const temp = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = temp;
 
-        if (hasSwapped === false) {
-            return array;
-        }
+        hasSwapped = true;
+      }
     }
 
+    if (!hasSwapped) {
+      return array;
+    }
+  }
 
-    return array;
+  return array;
 };
--- src/sorting/bubble/bubble.test.js
+++ src/sorting/bubble/bubble.test.js
@@ -1,66 +1,53 @@
-import {sortByBubble, sortByInvertedBubble} from './bubble.sort';
+import { sortByBubble, sortByInvertedBubble } from './bubble.sort';
 
 describe('[SORT] bubble', () => {
-    describe('when normal', () => {
-        test('should sort array ', () => {
-            expect(sortByBubble([
-                1, 3, 5, 4, 2
-            ])).toEqual([
-                1, 2, 3, 4, 5
-            ]);
-        });
-
-        test('should return already sorted array without changes', () => {
-            const array = [1, 2, 3, 4, 5];
-            expect(sortByBubble(array)).toEqual(array);
-        });
-
-        test('should return sorted array when array is sorted in reverse order', () => {
-            expect(sortByBubble([
-                5, 4, 3, 2, 1
-            ])).toEqual([
-                1, 2, 3, 4, 5
-            ]);
-        });
-
-        test('should return sorted array with repeated values', () => {
-            expect(sortByBubble([
-                5, 4, 3, 7, 9, 4
-            ])).toEqual([
-                3, 4, 4, 5, 7, 9
-            ])
-        });
+  describe('when normal', () => {
+    test('should sort array ', () => {
+      const unsorted = [1, 3, 5, 4, 2];
+      const sorted = [1, 2, 3, 4, 5];
+      expect(sortByBubble(unsorted)).toEqual(sorted);
     });
 
+    test('should return already sorted array without changes', () => {
+      const sorted = [1, 2, 3, 4, 5];
+      expect(sortByBubble(sorted)).toEqual(sorted);
+    });
 
-    describe('when inverted', () => {
-        test('should sort array ', () => {
-            expect(sortByInvertedBubble([
-                1, 3, 5, 4, 2
-            ])).toEqual([
-                1, 2, 3, 4, 5
-            ]);
-        });
+    test('should return sorted array when array is sorted in reverse order', () => {
+      const unsorted = [5, 4, 3, 2, 1];
+      const sorted = [1, 2, 3, 4, 5];
+      expect(sortByBubble(unsorted)).toEqual(sorted);
+    });
 
-        test('should return already sorted array without changes', () => {
-            const array = [1, 2, 3, 4, 5];
-            expect(sortByInvertedBubble(array)).toEqual(array);
-        });
+    test('should return sorted array with repeated values', () => {
+      const unsorted = [5, 4, 3, 7, 9, 4];
+      const sorted = [3, 4, 4, 5, 7, 9];
+      expect(sortByBubble(unsorted)).toEqual(sorted);
+    });
+  });
 
-        test('should return sorted array when array is sorted in reverse order', () => {
-            expect(sortByInvertedBubble([
-                5, 4, 3, 2, 1
-            ])).toEqual([
-                1, 2, 3, 4, 5
-            ]);
-        });
+  describe('when inverted', () => {
+    test('should sort array ', () => {
+      const unsorted = [1, 3, 5, 4, 2];
+      const sorted = [1, 2, 3, 4, 5];
+      expect(sortByInvertedBubble(unsorted)).toEqual(sorted);
+    });
 
-        test('should return sorted array with repeated values', () => {
-            expect(sortByInvertedBubble([
-                5, 4, 3, 7, 9, 4
-            ])).toEqual([
-                3, 4, 4, 5, 7, 9
-            ])
-        });
-    })
+    test('should return already sorted array without changes', () => {
+      const sorted = [1, 2, 3, 4, 5];
+      expect(sortByInvertedBubble(sorted)).toEqual(sorted);
+    });
+
+    test('should return sorted array when array is sorted in reverse order', () => {
+      const unsorted = [5, 4, 3, 2, 1];
+      const sorted = [1, 2, 3, 4, 5];
+      expect(sortByInvertedBubble(unsorted)).toEqual(sorted);
+    });
+
+    test('should return sorted array with repeated values', () => {
+      const unsorted = [5, 4, 3, 7, 9, 4];
+      const sorted = [3, 4, 4, 5, 7, 9];
+      expect(sortByInvertedBubble(unsorted)).toEqual(sorted);
+    });
+  });
 });
