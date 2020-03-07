6ed9c4ec02bd049b30780ef1217c0c021eacdef2
maktarsis <tarsis.maksym@gmail.com>
2020-03-07 19:34:53 +0200

    refactor selection sort

--- src/sorting/selection/selection.sort.js
+++ src/sorting/selection/selection.sort.js
@@ -3,20 +3,19 @@
  * Complexity: O(N^2)
  */
 export const sortBySelection = array => {
-    const n = array.length;
+  for (let i = 0; i < array.length; i += 1) {
+    let minId = i;
 
-    for (let i = 0; i < n; i += 1) {
-        let minId = i;
-        for (let j = i + 1; j < n; j += 1) {
-            if (array[j] < array[minId]) {
-                minId = j;
-            }
-        }
-
-        const temp = array[i];
-        array[i] = array[minId];
-        array[minId] = temp;
+    for (let j = i + 1; j < array.length; j += 1) {
+      if (array[j] < array[minId]) {
+        minId = j;
+      }
     }
 
-    return array;
+    const temp = array[i];
+    array[i] = array[minId];
+    array[minId] = temp;
+  }
+
+  return array;
 };
--- src/sorting/selection/selection.test.js
+++ src/sorting/selection/selection.test.js
@@ -2,19 +2,25 @@ import { sortBySelection } from './selection.sort';
 
 describe('[SORT] selection', () => {
   test('should sort array ', () => {
-    expect(sortBySelection([1, 3, 5, 4, 2])).toEqual([1, 2, 3, 4, 5]);
+    const unsorted = [1, 3, 5, 4, 2];
+    const sorted = [1, 2, 3, 4, 5];
+    expect(sortBySelection(unsorted)).toEqual(sorted);
   });
 
   test('should return already sorted array without changes', () => {
-    const array = [1, 2, 3, 4, 5];
-    expect(sortBySelection(array)).toEqual(array);
+    const sorted = [1, 2, 3, 4, 5];
+    expect(sortBySelection(sorted)).toEqual(sorted);
   });
 
   test('should return sorted array when array is sorted in reverse order', () => {
-    expect(sortBySelection([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+    const unsorted = [5, 4, 3, 2, 1];
+    const sorted = [1, 2, 3, 4, 5];
+    expect(sortBySelection(unsorted)).toEqual(sorted);
   });
 
   test('should return sorted array with repeated values', () => {
-    expect(sortBySelection([5, 4, 3, 7, 9, 4])).toEqual([3, 4, 4, 5, 7, 9]);
+    const unsorted = [5, 4, 3, 7, 9, 4];
+    const sorted = [3, 4, 4, 5, 7, 9];
+    expect(sortBySelection(unsorted)).toEqual(sorted);
   });
 });
