5675a49a3528615b0a72115137cc6e8bcd151008
maktarsis <tarsis.maksym@gmail.com>
2020-03-07 18:17:21 +0200

    add linter, commit hooks, code formatter

--- src/sorting/selection/selection.test.js
+++ src/sorting/selection/selection.test.js
@@ -1,32 +1,20 @@
-import {sortBySelection} from './selection.sort';
+import { sortBySelection } from './selection.sort';
 
 describe('[SORT] selection', () => {
-    test('should sort array ', () => {
-        expect(sortBySelection([
-            1, 3, 5, 4, 2
-        ])).toEqual([
-            1, 2, 3, 4, 5
-        ]);
-    });
+  test('should sort array ', () => {
+    expect(sortBySelection([1, 3, 5, 4, 2])).toEqual([1, 2, 3, 4, 5]);
+  });
 
-    test('should return already sorted array without changes', () => {
-        const array = [1, 2, 3, 4, 5];
-        expect(sortBySelection(array)).toEqual(array);
-    });
+  test('should return already sorted array without changes', () => {
+    const array = [1, 2, 3, 4, 5];
+    expect(sortBySelection(array)).toEqual(array);
+  });
 
-    test('should return sorted array when array is sorted in reverse order', () => {
-        expect(sortBySelection([
-            5, 4, 3, 2, 1
-        ])).toEqual([
-            1, 2, 3, 4, 5
-        ]);
-    });
+  test('should return sorted array when array is sorted in reverse order', () => {
+    expect(sortBySelection([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+  });
 
-    test('should return sorted array with repeated values', () => {
-        expect(sortBySelection([
-            5, 4, 3, 7, 9, 4
-        ])).toEqual([
-            3, 4, 4, 5, 7, 9
-        ])
-    });
+  test('should return sorted array with repeated values', () => {
+    expect(sortBySelection([5, 4, 3, 7, 9, 4])).toEqual([3, 4, 4, 5, 7, 9]);
+  });
 });
