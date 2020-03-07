a67db1902d5ef3c6ac641e2250d6c55b0f1e97e6
maktarsis <tarsis.maksym@gmail.com>
2020-03-07 19:26:28 +0200

    refactor merge sort

--- src/sorting/merge/merge.sort.js
+++ src/sorting/merge/merge.sort.js
@@ -3,35 +3,31 @@
  * Complexity: O(N*log(N))
  */
 export const sortByMerge = array => {
-    if (array.length === 1) return array;
+  if (array.length < 2) {
+    return array;
+  }
 
-    const divideId = Math.floor(array.length / 2);
-    const left = array.slice(0, divideId);
-    const right = array.slice(divideId);
+  const delimiter = Math.floor(array.length / 2);
+  const left = array.slice(0, delimiter);
+  const right = array.slice(delimiter);
 
-    return merge(
-        sortByMerge(left),
-        sortByMerge(right)
-    );
+  return merge(sortByMerge(left), sortByMerge(right));
 };
 
 function merge(left, right) {
-    const sortedArray = [];
-    let leftId = 0;
-    let rightId = 0;
+  const sorted = [];
+  let leftId = 0;
+  let rightId = 0;
 
-    while(leftId < left.length && rightId < right.length) {
-        if (left[leftId] < right[rightId]) {
-            sortedArray.push(left[leftId]);
-            leftId += 1;
-        } else {
-            sortedArray.push(right[rightId]);
-            rightId += 1;
-        }
+  while (leftId < left.length && rightId < right.length) {
+    if (left[leftId] < right[rightId]) {
+      sorted.push(left[leftId]);
+      leftId += 1;
+    } else {
+      sorted.push(right[rightId]);
+      rightId += 1;
     }
+  }
 
-    return sortedArray.concat(
-        left.slice(leftId),
-        right.slice(rightId)
-    )
+  return sorted.concat(left.slice(leftId), right.slice(rightId));
 }
--- src/sorting/merge/merge.test.js
+++ src/sorting/merge/merge.test.js
@@ -1,32 +1,26 @@
-import {sortByMerge} from './merge.sort';
+import { sortByMerge } from './merge.sort';
 
 describe('[SORT] merge', () => {
-    test('should sort array ', () => {
-        expect(sortByMerge([
-            1, 3, 5, 4, 2
-        ])).toEqual([
-            1, 2, 3, 4, 5
-        ]);
-    });
+  test('should sort array ', () => {
+    const unsorted = [1, 3, 5, 4, 2];
+    const sorted = [1, 2, 3, 4, 5];
+    expect(sortByMerge(unsorted)).toEqual(sorted);
+  });
 
-    test('should return already sorted array without changes', () => {
-        const array = [1, 2, 3, 4, 5];
-        expect(sortByMerge(array)).toEqual(array);
-    });
+  test('should return already sorted array without changes', () => {
+    const sorted = [1, 2, 3, 4, 5];
+    expect(sortByMerge(sorted)).toEqual(sorted);
+  });
 
-    test('should return sorted array when array is sorted in reverse order', () => {
-        expect(sortByMerge([
-            5, 4, 3, 2, 1
-        ])).toEqual([
-            1, 2, 3, 4, 5
-        ]);
-    });
+  test('should return sorted array when array is sorted in reverse order', () => {
+    const unsorted = [5, 4, 3, 2, 1];
+    const sorted = [1, 2, 3, 4, 5];
+    expect(sortByMerge(unsorted)).toEqual(sorted);
+  });
 
-    test('should return sorted array with repeated values', () => {
-        expect(sortByMerge([
-            5, 4, 3, 7, 9, 4
-        ])).toEqual([
-            3, 4, 4, 5, 7, 9
-        ])
-    });
+  test('should return sorted array with repeated values', () => {
+    const unsorted = [5, 4, 3, 7, 9, 4];
+    const sorted = [3, 4, 4, 5, 7, 9];
+    expect(sortByMerge(unsorted)).toEqual(sorted);
+  });
 });
