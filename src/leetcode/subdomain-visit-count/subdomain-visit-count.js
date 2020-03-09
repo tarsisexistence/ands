089e91e962f6d2ede001eb9aab254fb718788f7c
maktarsis <tarsis.maksym@gmail.com>
2020-03-09 19:39:35 +0200

    add subdomain-visit-count solution

---
+++ src/leetcode/subdomain-visit-count/subdomain-visit-count.js
@@ -0,0 +1,41 @@
+/**
+ * Leetcode #811
+ * Difficulty: Easy
+ * https://leetcode.com/problems/subdomain-visit-count/
+ */
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ */
+export const subdomainVisits = cpdomains => {
+  const visitsEntity = {};
+  const result = [];
+
+  for (let i = 0; i < cpdomains.length; i += 1) {
+    let [visits, domain] = cpdomains[i].split(' ');
+
+    while (domain.length > 0) {
+      recordDomain(visits, domain);
+      const domainSeparatorId = domain.indexOf('.');
+      domain =
+        domainSeparatorId === -1 ? '' : domain.slice(domainSeparatorId + 1);
+    }
+  }
+
+  const totalDomains = Object.keys(visitsEntity);
+
+  for (let i = 0; i < totalDomains.length; i += 1) {
+    const domain = totalDomains[i];
+    result.push(`${visitsEntity[domain]} ${domain}`);
+  }
+
+  return result;
+
+  function recordDomain(visits, domain) {
+    visits = Number(visits);
+    visitsEntity[domain] =
+      visitsEntity[domain] === undefined
+        ? visits
+        : visitsEntity[domain] + visits;
+  }
+};
---
+++ src/leetcode/subdomain-visit-count/subdomain-visit-count.test.js
@@ -0,0 +1,30 @@
+import { subdomainVisits } from './subdomain-visit-count';
+
+describe('[Leetcode] subdomainVisits', () => {
+  test('example #1', () => {
+    expect(subdomainVisits(['9001 discuss.leetcode.com'])).toEqual([
+      '9001 discuss.leetcode.com',
+      '9001 leetcode.com',
+      '9001 com'
+    ]);
+  });
+
+  test('example #2', () => {
+    expect(
+      subdomainVisits([
+        '900 google.mail.com',
+        '50 yahoo.com',
+        '1 intel.mail.com',
+        '5 wiki.org'
+      ])
+    ).toEqual([
+      '900 google.mail.com',
+      '901 mail.com',
+      '951 com',
+      '50 yahoo.com',
+      '1 intel.mail.com',
+      '5 wiki.org',
+      '5 org'
+    ]);
+  });
+});
