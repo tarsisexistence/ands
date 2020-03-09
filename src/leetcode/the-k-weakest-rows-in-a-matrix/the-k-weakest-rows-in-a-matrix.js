43d773a4d9aa677b3a25750599332ef2f02afbab
maktarsis <tarsis.maksym@gmail.com>
2020-03-09 14:37:13 +0200

    add solution for k-weakest-rows-in-a-matrix

---
+++ src/leetcode/the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.js
@@ -0,0 +1,18 @@
+/**
+ * Leetcode #1337
+ * Difficulty: Easy
+ * https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+ */
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+export const kWeakestRows = (mat, k) =>
+  mat
+    .map((m, i) => ({ i, soldiers: m.reduce((acc, bin) => acc + bin, 0) }))
+    .sort((a, b) =>
+      a.soldiers !== b.soldiers ? a.soldiers - b.soldiers : a.i - b.i
+    )
+    .map(v => v.i)
+    .slice(0, k);
