4a4df48470347400fc50dfa0cdbf6fbb8b808dac
maktarsis <tarsis.maksym@gmail.com>
2020-04-04 14:36:53 +0300

    add more info about maximum-subarray problem + add test cases for it

--- src/leetcode/maximum-subarray/maximum-subarray.js
+++ src/leetcode/maximum-subarray/maximum-subarray.js
@@ -2,14 +2,15 @@
  * Leetcode #53
  * Difficulty: Easy
  * https://leetcode.com/problems/maximum-subarray/
+ * Kadane's algorithm
  */
 /**
  * @param {number[]} nums
  * @return {number}
  */
 export const maxSubArray = nums => {
-  let currentSum = nums[0];
-  let maxSum = currentSum;
+  let maxSum = nums[0];
+  let currentSum = maxSum;
 
   for (let i = 1; i < nums.length; i += 1) {
     currentSum = Math.max(nums[i], currentSum + nums[i]);
@@ -18,3 +19,16 @@ export const maxSubArray = nums => {
 
   return maxSum;
 };
+
+// export const maxSubArray = nums => {
+//   let max = nums[0];
+//   let current = Math.max(max, 0);
+//
+//   for (let i = 1; i < nums.length; i += 1) {
+//     current += nums[i];
+//     max = Math.max(max, current);
+//     current = Math.max(current, 0);
+//   }
+//
+//   return max;
+// };
--- src/leetcode/maximum-subarray/maximum-subarray.test.js
+++ src/leetcode/maximum-subarray/maximum-subarray.test.js
@@ -4,4 +4,28 @@ describe('[Leetcode] maxSubArray', () => {
   test('example #1', () => {
     expect(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6);
   });
+
+  test('example #2', () => {
+    expect(maxSubArray([-1])).toBe(-1);
+  });
+
+  test('example #3', () => {
+    expect(maxSubArray([1])).toBe(1);
+  });
+
+  test('example #4', () => {
+    expect(maxSubArray([-2, 1])).toBe(1);
+  });
+
+  test('example #5', () => {
+    expect(maxSubArray([-2, -3, 4, -1, -2, 1, 5, -3])).toBe(7);
+  });
+
+  test('example #6', () => {
+    expect(maxSubArray([10, -3, 5])).toBe(12);
+  });
+
+  test('example #7', () => {
+    expect(maxSubArray([-10, -3, -5, -2, -6])).toBe(-2);
+  });
 });
