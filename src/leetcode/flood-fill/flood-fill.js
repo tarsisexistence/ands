5e4ce6fb7fdb4bcef395eb960b17d90f0dcd10b8
maktarsis <tarsis.maksym@gmail.com>
2020-05-11 16:35:48 +0300

    add flood-fill (100th solved leetcode problem)

---
+++ src/leetcode/flood-fill/flood-fill.js
@@ -0,0 +1,38 @@
+/**
+ * Leetcode #997
+ * Difficulty: Easy
+ * https://leetcode.com/problems/find-the-town-judge/
+ */
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+export const floodFill = (image, sr, sc, newColor) => {
+  const filledMap = new Map();
+  const targetColor = image[sr][sc];
+  flood(sr, sc);
+  return image;
+
+  function flood(r, c) {
+    const tag = `${r}.${c}`;
+
+    if (
+      image[r] === undefined ||
+      image[r][c] !== targetColor ||
+      filledMap.has(tag)
+    ) {
+      return;
+    }
+
+    filledMap.set(tag, true);
+    image[r][c] = newColor;
+
+    flood(r - 1, c);
+    flood(r + 1, c);
+    flood(r, c - 1);
+    flood(r, c + 1);
+  }
+};
---
+++ src/leetcode/flood-fill/flood-fill.test.js
@@ -0,0 +1,41 @@
+import { floodFill } from './flood-fill';
+
+describe('[Leetcode] floodFill', () => {
+  test('example #1', () => {
+    expect(
+      floodFill(
+        [
+          [1, 1, 1],
+          [1, 1, 0],
+          [1, 0, 1]
+        ],
+        1,
+        1,
+        2
+      )
+    ).toEqual([
+      [2, 2, 2],
+      [2, 2, 0],
+      [2, 0, 1]
+    ]);
+  });
+
+  test('example #1', () => {
+    expect(
+      floodFill(
+        [
+          [1, 1, 2],
+          [2, 2, 0],
+          [2, 0, 2]
+        ],
+        1,
+        1,
+        3
+      )
+    ).toEqual([
+      [1, 1, 2],
+      [3, 3, 0],
+      [3, 0, 2]
+    ]);
+  });
+});
