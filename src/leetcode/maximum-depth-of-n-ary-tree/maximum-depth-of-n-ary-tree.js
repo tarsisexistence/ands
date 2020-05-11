a5913bb1b6e0a00d77c5e854b9b6cb34e9248eeb
maktarsis <tarsis.maksym@gmail.com>
2020-05-12 00:19:31 +0300

    add maximum-depth-of-n-ary-tree solution

---
+++ src/leetcode/maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.js
@@ -0,0 +1,42 @@
+/**
+ * Leetcode #559
+ * Difficulty: Easy
+ * https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+ */
+
+/**
+ * @param {Node} root
+ * @return {number}
+ */
+export const maxDepth = root => {
+  let depth = 0;
+
+  if (root === null) {
+    return depth;
+  }
+
+  for (let i = 0; i < root.children.length; i += 1) {
+    depth = Math.max(depth, maxDepth(root.children[i]));
+  }
+
+  return depth + 1;
+};
+
+// const maxDepth = root => {
+//   if (root === null) {
+//     return 0;
+//   }
+//
+//   let count = 1;
+//   dfs(root, count)
+//
+//   return count;
+//
+//   function dfs(node, depth) {
+//     count = Math.max(count, depth);
+//
+//     node.children.forEach(child => {
+//       dfs(child, depth + 1);
+//     });
+//   }
+// };
--- src/utils/node.js
+++ src/utils/node.js
@@ -7,3 +7,8 @@ export function TreeNode(val) {
   this.val = val;
   this.left = this.right = null;
 }
+
+export function Node(val, children) {
+  this.val = val;
+  this.children = children;
+}
