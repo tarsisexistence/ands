5bdbf430a9958f5c9b91aaa5432308cbd54f05c5
maktarsis <tarsis.maksym@gmail.com>
2020-05-22 11:14:10 +0300

    refactor count-primes

--- src/leetcode/count-primes/count-primes.js
+++ src/leetcode/count-primes/count-primes.js
@@ -1,4 +1,9 @@
 /**
+ * Leetcode #204
+ * Difficulty: Easy
+ * https://leetcode.com/problems/count-primes/
+ */
+/**
  * @param {number} n
  * @return {number}
  */
@@ -17,3 +22,19 @@ export const countPrimes = (n) => {
 
   return nums.filter((val) => val).length;
 };
+
+// export const countPrimes = n => {
+//   const nums = new Array(n).fill(true);
+//
+//   for (let i = 2; i * i < n; i += 1) {
+//     if (nums[i] === false) {
+//       continue;
+//     }
+//
+//     for (let j = i * i; j < n; j += i) {
+//       nums[j] = false;
+//     }
+//   }
+//
+//   return nums.filter(num => num).length - 2;
+// };
