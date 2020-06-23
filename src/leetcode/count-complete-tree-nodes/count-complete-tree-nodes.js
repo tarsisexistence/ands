2ea41f4412fa513a57d43324c66802c2ea62ca8f
maktarsis <tarsis.maksym@gmail.com>
2020-06-23 23:26:49 +0300

    add count-complete-tree-nodes solution

---
+++ src/leetcode/count-complete-tree-nodes/count-complete-tree-nodes.js
@@ -0,0 +1,11 @@
+/**
+ * Leetcode #222
+ * Difficulty: Easy
+ * https://leetcode.com/problems/count-complete-tree-nodes/
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+export const countNodes = root =>
+  root === null ? 0 : 1 + countNodes(root.left) + countNodes(root.right);
---
+++ src/leetcode/count-complete-tree-nodes/count-complete-tree-nodes.test.js
@@ -0,0 +1,24 @@
+import { countNodes } from './count-complete-tree-nodes';
+import { TreeNode } from '../../utils/node';
+
+describe('[Leetcode] countNodes', () => {
+  test('example #1', () => {
+    expect(
+      countNodes(
+        new TreeNode(
+          1,
+          new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+          new TreeNode(3, new TreeNode(6))
+        )
+      )
+    ).toBe(6);
+  });
+
+  test('example #2', () => {
+    expect(countNodes(null)).toBe(0);
+  });
+
+  test('example #3', () => {
+    expect(countNodes(new TreeNode(10))).toBe(1);
+  });
+});
