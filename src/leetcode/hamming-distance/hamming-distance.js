cc58f9a16b493be10e2615da9c3096d6bd0e1d48
maktarsis <tarsis.maksym@gmail.com>
2020-07-05 18:57:30 +0300

    add hamming-distance (new solution) and add test

--- src/leetcode/hamming-distance/hamming-distance.js
+++ src/leetcode/hamming-distance/hamming-distance.js
@@ -9,21 +9,39 @@
  * @return {number}
  */
 export const hammingDistance = (x, y) => {
-  let binX = x.toString(2);
-  let binY = y.toString(2);
-  let differences = 0;
+  let distance = 0;
+  const left = x.toString(2).split('').reverse();
+  const right = y.toString(2).split('').reverse();
+  const longest = Math.max(left.length, right.length);
 
-  if (binX.length > binY.length) {
-    binY = Array(binX.length - binY.length).fill('0').join('') + binY;
-  } else {
-    binX = Array(binY.length - binX.length).fill('0').join('') + binX;
-  }
+  for (let i = 0; i < longest; i += 1) {
+    const isLeftOne = left[i] === '1';
+    const isRightOne = right[i] === '1';
 
-  for (let i = 0; i < binX.length; i += 1) {
-    if (binX[i] !== binY[i]) {
-      differences += 1;
+    if (isLeftOne !== isRightOne) {
+      distance += 1;
     }
   }
 
-  return differences;
+  return distance;
 };
+
+// export const hammingDistance = (x, y) => {
+//   let binX = x.toString(2);
+//   let binY = y.toString(2);
+//   let differences = 0;
+//
+//   if (binX.length > binY.length) {
+//     binY = Array(binX.length - binY.length).fill('0').join('') + binY;
+//   } else {
+//     binX = Array(binY.length - binX.length).fill('0').join('') + binX;
+//   }
+//
+//   for (let i = 0; i < binX.length; i += 1) {
+//     if (binX[i] !== binY[i]) {
+//       differences += 1;
+//     }
+//   }
+//
+//   return differences;
+// };
---
+++ src/leetcode/hamming-distance/hamming-distance.test.js
@@ -0,0 +1,7 @@
+import { hammingDistance } from './hamming-distance';
+
+describe('[Leetcode] hammingDistance', () => {
+  test('example #1', () => {
+    expect(hammingDistance(1, 4)).toBe(2);
+  });
+});
