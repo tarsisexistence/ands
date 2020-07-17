4aebcd99118c1b3c50cd663ac05162d0f4c6549d
maktarsis <tarsis.maksym@gmail.com>
2020-07-17 15:02:11 +0300

    add more efficient sum-of-even-numbers-after-queries solution

--- src/leetcode/sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.js
+++ src/leetcode/sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.js
@@ -10,16 +10,26 @@
  */
 export const sumEvenAfterQueries = (A, queries) => {
   const result = new Array(A.length);
+  let sum = 0;
+
+  for (let j = 0; j < A.length; j += 1) {
+    if (A[j] % 2 === 0) {
+      sum += A[j];
+    }
+  }
 
   for (let i = 0; i < queries.length; i += 1) {
     const [inc, pos] = queries[i];
-    let sum = 0;
+    const prev = A[pos];
     A[pos] += inc;
+    const next = A[pos];
+    const isPrevEven = prev % 2 === 0;
+    const isNextEven = next % 2 === 0;
 
-    for (let j = 0; j < A.length; j += 1) {
-      if (A[j] % 2 === 0) {
-        sum += A[j];
-      }
+    if (isNextEven) {
+      sum += isPrevEven ? inc : next;
+    } else if (isPrevEven) {
+      sum -= prev;
     }
 
     result[i] = sum;
@@ -27,3 +37,23 @@ export const sumEvenAfterQueries = (A, queries) => {
 
   return result;
 };
+
+// export const sumEvenAfterQueries = (A, queries) => {
+//   const result = new Array(A.length);
+//
+//   for (let i = 0; i < queries.length; i += 1) {
+//     const [inc, pos] = queries[i];
+//     let sum = 0;
+//     A[pos] += inc;
+//
+//     for (let j = 0; j < A.length; j += 1) {
+//       if (A[j] % 2 === 0) {
+//         sum += A[j];
+//       }
+//     }
+//
+//     result[i] = sum;
+//   }
+//
+//   return result;
+// };
