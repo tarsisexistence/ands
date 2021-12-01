4f23b8221974603b09ced9005efc3b1d6f07dce9
Max Tarsis <21989873+untaggable@users.noreply.github.com>
2021-12-01 10:24:44 +0200

    refactor: update prev solution

--- src/leetcode/count-of-matches-in-tournament/count-of-matches-in-tournament.js
+++ src/leetcode/count-of-matches-in-tournament/count-of-matches-in-tournament.js
@@ -10,16 +10,28 @@
 export const numberOfMatches = n => {
   let matches = 0;
 
-  while (true) {
+  while (n > 1) {
     const value = n / 2;
-
-    if (value < 1) {
-      return matches;
-    }
-
     matches += Math.floor(value);
     n = Math.ceil(value);
   }
+
+  return matches;
 };
 
 // const numberOfMatches = n => n - 1;
+
+// const numberOfMatches = n => {
+//   let matches = 0;
+//
+//   while (true) {
+//     const value = n / 2;
+//
+//     if (value < 1) {
+//       return matches;
+//     }
+//
+//     matches += Math.floor(value);
+//     n = Math.ceil(value);
+//   }
+// };
