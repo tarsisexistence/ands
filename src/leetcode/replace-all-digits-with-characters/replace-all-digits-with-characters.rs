1e0ed37b2277913739e8923a1cb24f2567c79237
Max Tarsis <21989873+untaggable@users.noreply.github.com>
2021-12-17 13:56:12 +0200

    feat: update replace-all-digits-with-characters.rs solution in rust

--- src/leetcode/replace-all-digits-with-characters/replace-all-digits-with-characters.rs
+++ src/leetcode/replace-all-digits-with-characters/replace-all-digits-with-characters.rs
@@ -1,16 +1,31 @@
 impl Solution {
-    pub fn replace_digits(s: String) -> String {
-        let mut chars: Vec<char> = s.chars().collect();
-
+    pub fn replace_digits(mut s: String) -> String {
         for idx in 1..s.len() {
             if idx % 2 == 1 {
-                let prev: u8 = chars[idx - 1] as u8 - 'a' as u8;
-                let shift: u8 = chars[idx] as u8 - '0' as u8;
+                let prev: u8 = s.chars().nth(idx - 1).unwrap() as u8 - 'a' as u8;
+                let shift: u8 = s.chars().nth(idx).unwrap() as u8 - '0' as u8;
                 let char: char = (prev + shift + 'a' as u8) as char;
-                chars[idx] = char;
+                s.replace_range(idx..idx+1, &char.to_string());
             }
         }
 
-        chars.iter().collect::<String>()
+        s
     }
 }
+
+// impl Solution {
+//     pub fn replace_digits(s: String) -> String {
+//         let mut chars: Vec<char> = s.chars().collect();
+//
+//         for idx in 1..s.len() {
+//             if idx % 2 == 1 {
+//                 let prev: u8 = chars[idx - 1] as u8 - 'a' as u8;
+//                 let shift: u8 = chars[idx] as u8 - '0' as u8;
+//                 let char: char = (prev + shift + 'a' as u8) as char;
+//                 chars[idx] = char;
+//             }
+//         }
+//
+//         chars.iter().collect::<String>()
+//     }
+// }
