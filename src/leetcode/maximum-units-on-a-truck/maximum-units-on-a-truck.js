e6db80277633a52093d42d374f9f2575f755a0a3
tarsinzer <tarsis.maksym@gmail.com>
2022-07-01 20:08:58 +0100

    feat: solve maximum-units-on-a-truck js [2]

--- src/leetcode/maximum-units-on-a-truck/maximum-units-on-a-truck.js
+++ src/leetcode/maximum-units-on-a-truck/maximum-units-on-a-truck.js
@@ -9,23 +9,40 @@
  * @return {number}
  */
 export const maximumUnits = (boxTypes, truckSize) => {
-  let max = 0;
+  let result = 0;
 
   boxTypes.sort((a, b) => b[1] - a[1]);
 
-  for (let i = 0; i < boxTypes.length; i += 1) {
-    let [boxes, units] = boxTypes[i];
+  for (let i = 0; i < boxTypes.length && truckSize > 0; i += 1) {
+    const [boxes, units] = boxTypes[i];
 
-    while (boxes > 0 && truckSize > 0) {
-      boxes -= 1;
-      truckSize -= 1;
-      max += units;
-    }
-
-    if (truckSize === 0) {
-      break;
-    }
+    const newTruckSize = Math.max(truckSize - boxes, 0);
+    const diff = truckSize - newTruckSize;
+    truckSize = newTruckSize;
+    result += diff * units;
   }
 
-  return max;
+  return result;
 };
+
+// export const maximumUnits = (boxTypes, truckSize) => {
+//   let max = 0;
+//
+//   boxTypes.sort((a, b) => b[1] - a[1]);
+//
+//   for (let i = 0; i < boxTypes.length; i += 1) {
+//     let [boxes, units] = boxTypes[i];
+//
+//     while (boxes > 0 && truckSize > 0) {
+//       boxes -= 1;
+//       truckSize -= 1;
+//       max += units;
+//     }
+//
+//     if (truckSize === 0) {
+//       break;
+//     }
+//   }
+//
+//   return max;
+// };
