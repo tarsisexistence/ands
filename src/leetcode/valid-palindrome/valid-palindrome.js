31734206fea974a494c897db54467816d4f8f286
tarsinzer <tarsis.maksym@gmail.com>
2022-07-23 00:13:43 +0100

    feat: solve valid-palindrome in a different way

--- src/leetcode/valid-palindrome/valid-palindrome.js
+++ src/leetcode/valid-palindrome/valid-palindrome.js
@@ -7,8 +7,25 @@
  * @param {string} s
  * @return {boolean}
  */
+// export const isPalindrome = s => {
+//   const chars = [];
+//
+//   for (let i = 0; i < s.length; i += 1) {
+//     const char = s[i].toLowerCase();
+//     const code = char.charCodeAt();
+//     const isNumerical = code >= 48 && code <= 57;
+//     const isAlphabetical = code >= 97 && code <= 122;
+//
+//     if (isNumerical || isAlphabetical) {
+//       chars.push(char);
+//     }
+//   }
+//
+//   return chars.join('') === chars.reverse().join('');
+// };
+
 export const isPalindrome = s => {
-  const chars = [];
+  let chars = '';
 
   for (let i = 0; i < s.length; i += 1) {
     const char = s[i].toLowerCase();
@@ -17,9 +34,15 @@ export const isPalindrome = s => {
     const isAlphabetical = code >= 97 && code <= 122;
 
     if (isNumerical || isAlphabetical) {
-      chars.push(char);
+      chars += char;
     }
   }
 
-  return chars.join('') === chars.reverse().join('');
+  for (let i = 0; i < chars.length; i += 1) {
+    if (chars[i] !== chars[chars.length - i - 1]) {
+      return false;
+    }
+  }
+
+  return true;
 };
