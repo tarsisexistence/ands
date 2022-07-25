682a923991ed87780e80890b3d40301296f2172b
tarsinzer <tarsis.maksym@gmail.com>
2022-07-25 23:46:39 +0100

    feat: solve ransom-note again

--- src/leetcode/ransom-note/ransom-note.js
+++ src/leetcode/ransom-note/ransom-note.js
@@ -9,24 +9,21 @@
  * @return {boolean}
  */
 export const canConstruct = (ransomNote, magazine) => {
-  const noteMap = new Map();
-
-  for (let i = 0; i < ransomNote.length; i += 1) {
-    const char = ransomNote[i];
-    const quantity = noteMap.get(char);
-    noteMap.set(char, 1 + (quantity || 0));
-  }
+  const details = new Map();
 
   for (let i = 0; i < magazine.length; i += 1) {
-    const char = magazine[i];
-    const remaining = (noteMap.get(char) || 0) - 1;
-
-    if (remaining > 0) {
-      noteMap.set(char, remaining);
-    } else {
-      noteMap.delete(char);
-    }
+    details.set(magazine[i], (details.get(magazine[i]) ?? 0) + 1);
   }
 
-  return noteMap.size === 0;
+  for (let i = 0; i < ransomNote.length; i += 1) {
+    const remaining = details.get(ransomNote[i]);
+
+    if (!remaining) {
+      return false;
+    }
+
+    details.set(ransomNote[i], remaining - 1);
+  }
+
+  return true;
 };
