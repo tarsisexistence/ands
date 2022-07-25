592de964d34d8e546f7bc9fe67ce3798eb39ab71
tarsinzer <tarsis.maksym@gmail.com>
2022-07-25 11:12:41 +0100

    fix: solve implement-queue-using-stacks in according to the constraints

--- src/leetcode/implement-queue-using-stacks/implement-queue-using-stacks.js
+++ src/leetcode/implement-queue-using-stacks/implement-queue-using-stacks.js
@@ -5,7 +5,8 @@
  */
 
 const MyQueue = function () {
-  this.queue = [];
+  this.stack1 = [];
+  this.stack2 = [];
 };
 
 /**
@@ -13,26 +14,48 @@ const MyQueue = function () {
  * @return {void}
  */
 MyQueue.prototype.push = function (x) {
-  this.queue.push(x);
+  this.stack1.push(x);
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.pop = function () {
-  return this.queue.shift();
+  while (this.stack1.length !== 0) {
+    this.stack2.push(this.stack1.pop());
+  }
+
+  const el = this.stack2.pop();
+
+  while (this.stack2.length !== 0) {
+    this.stack1.push(this.stack2.pop());
+  }
+
+  return el;
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.peek = function () {
-  return this.queue[0];
+  // it's easier just to return `this.stack1.at(0)` but we're not allowed to
+  while (this.stack1.length !== 0) {
+    this.stack2.push(this.stack1.pop());
+  }
+
+  const el = this.stack2.pop();
+  this.stack2.push(el);
+
+  while (this.stack2.length !== 0) {
+    this.stack1.push(this.stack2.pop());
+  }
+
+  return el;
 };
 
 /**
  * @return {boolean}
  */
 MyQueue.prototype.empty = function () {
-  return this.queue.length === 0;
+  return this.stack1.length === 0;
 };
