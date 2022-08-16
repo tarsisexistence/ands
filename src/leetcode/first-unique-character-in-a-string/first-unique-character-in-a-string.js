9b9cbe71700e40b7fb0475849bc55a95d74b743f
tarsinzer <tarsis.maksym@gmail.com>
2022-08-16 05:01:59 +0100

    feat: solve first-unique-character-in-a-string again

--- src/leetcode/first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ src/leetcode/first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -11,11 +11,7 @@ export const firstUniqChar = s => {
   const map = new Map();
 
   for (let i = 0; i < s.length; i += 1) {
-    if (map.has(s[i])) {
-      map.set(s[i], 2);
-    } else {
-      map.set(s[i], 1);
-    }
+    map.set(s[i], (map.get(s[i]) ?? 0) + 1);
   }
 
   for (let i = 0; i < s.length; i += 1) {
@@ -26,3 +22,23 @@ export const firstUniqChar = s => {
 
   return -1;
 };
+
+// export const firstUniqChar = s => {
+//   const map = new Map();
+//
+//   for (let i = 0; i < s.length; i += 1) {
+//     if (map.has(s[i])) {
+//       map.set(s[i], 2);
+//     } else {
+//       map.set(s[i], 1);
+//     }
+//   }
+//
+//   for (let i = 0; i < s.length; i += 1) {
+//     if (map.get(s[i]) === 1) {
+//       return i;
+//     }
+//   }
+//
+//   return -1;
+// };
