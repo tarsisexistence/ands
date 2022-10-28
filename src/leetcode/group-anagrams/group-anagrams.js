45b8bbf19f9a4cabe25f343ca25fe6508763496b
Max Tarsis <21989873+tarsinzer@users.noreply.github.com>
2022-10-28 07:39:20 +0100

    feat: solve group-anagrams again (not efficiently)

--- src/leetcode/group-anagrams/group-anagrams.js
+++ src/leetcode/group-anagrams/group-anagrams.js
@@ -12,10 +12,7 @@ export const groupAnagrams = strs => {
 
   for (let i = 0; i < strs.length; i += 1) {
     const str = strs[i];
-    const anagram = str
-      .split('')
-      .sort()
-      .join('');
+    const anagram = str.split('').sort().join('');
 
     if (map.has(anagram)) {
       map.get(anagram).push(str);
@@ -26,3 +23,33 @@ export const groupAnagrams = strs => {
 
   return [...map.values()];
 };
+
+// const groupAnagrams = strs => {
+//   const res = [];
+//   const joins = [];
+//
+//   for (let i = 0; i < strs.length; i += 1) {
+//     const words = strs[i].split('').sort().join('');
+//     joins.push(words);
+//   }
+//
+//   for (let i = 0; i < strs.length; i += 1) {
+//     if (joins[i] === null) {
+//       continue;
+//     }
+//
+//     const arr = [strs[i]];
+//
+//     for (let j = i + 1; j < strs.length; j += 1) {
+//       if (joins[i] !== null && joins[i] === joins[j]) {
+//         arr.push(strs[j]);
+//         joins[j] = null;
+//       }
+//     }
+//
+//     joins[i] === null;
+//     res.push(arr);
+//   }
+//
+//   return res;
+// };
