1aa184592e8c62ffcb20d30f8dca86356915c68c
Max Tarsis <21989873+tarsinzer@users.noreply.github.com>
2022-11-01 09:54:15 +0000

    refactor: solve toeplitz-matrix again

--- src/leetcode/toeplitz-matrix/toeplitz-matrix.js
+++ src/leetcode/toeplitz-matrix/toeplitz-matrix.js
@@ -8,9 +8,9 @@
  * @return {boolean}
  */
 export const isToeplitzMatrix = matrix => {
-  for (let i = 1; i < matrix.length; i += 1) {
-    for (let j = 1; j < matrix[0].length; j += 1) {
-      if (matrix[i - 1][j - 1] !== matrix[i][j]) {
+  for (let i = 0; i < matrix.length - 1; i += 1) {
+    for (let j = 0; j < matrix[0].length - 1; j += 1) {
+      if (matrix[i][j] !== matrix[i + 1][j + 1]) {
         return false;
       }
     }
